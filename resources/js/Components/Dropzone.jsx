9d10137b576cae9f1a24a0dd1cb50f30c0b9585b
Moncef <laftim.moncef@gmail.com>
2023-12-05 19:36:32 +0100

    dropzone

--- resources/js/Components/Dropzone.jsx
+++ resources/js/Components/Dropzone.jsx
@@ -28,53 +28,58 @@ const DropZone = ({ files, upload, deleletFile, removeAll }) => {
     // }
 
     return (
-        <>
-            <Dropzone onDrop={handleOnDrop}>
-                {({ getRootProps, getInputProps }) => (
-                    <>
-                        <div className="col-lg-10">
-                            <a {...getRootProps({ className: 'dropzone' })} className="dropzone-select btn btn-sm btn-primary me-2">Attach files</a>
-                            {files && files.length > 0 ?
-                                <>
-                                    <a className="dropzone-remove-all btn btn-sm btn-light-primary" onClick={removeAll}>Remove all</a>
-                                    {files.map((file, i) => (
-                                        <div className="dropzone-items wm-200px mt-5" key={i}>
-                                            <div className="dropzone-item">
-                                                <div className="dropzone-file">
-                                                    <div className="dropzone-filename">
-                                                        <span>{file.name} </span>
-                                                        <strong>
-                                                            {"("}
-                                                            <span>
-                                                                <strong>{file.size} </strong> KB
-                                                            </span>
-                                                            {")"}
-                                                        </strong>
-                                                    </div>
-                                                </div>
-                                                <div className="dropzone-progress">
-                                                    <div className="progress">
-                                                        <div className="progress-bar bg-primary" role="progressbar">
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="dropzone-toolbar">
-                                                    <span className="dropzone-delete" onClick={() => deleletFile(i)}>
-                                                        <i className="bi bi-x fs-1"></i>
-                                                    </span>
+        <Dropzone onDrop={handleOnDrop}>
+            {({ getRootProps, getInputProps }) => (
+                <>
+                    <div className="col-lg-10">
+                        <a {...getRootProps({ className: 'dropzone' })} className="dropzone-select btn btn-sm btn-primary me-2">
+                            <input {...getInputProps()} />
+                            Attach files
+                        </a>
+                        {/* <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            Click me to upload a file!
+                        </div> */}
+                        {files && files.length > 0 ?
+                            <>
+                                <a className="dropzone-remove-all btn btn-sm btn-light-primary" onClick={removeAll}>Remove all</a>
+                                {files.map((file, i) => (
+                                    <div className="dropzone-items wm-200px mt-5" key={i}>
+                                        <div className="dropzone-item">
+                                            <div className="dropzone-file">
+                                                <div className="dropzone-filename">
+                                                    <span>{file.name} </span>
+                                                    <strong>
+                                                        {"("}
+                                                        <span>
+                                                            <strong>{file.size} </strong> KB
+                                                        </span>
+                                                        {")"}
+                                                    </strong>
                                                 </div>
                                             </div>
+                                            <div className="dropzone-progress">
+                                                <div className="progress">
+                                                    <div className="progress-bar bg-primary" role="progressbar">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="dropzone-toolbar">
+                                                <span className="dropzone-delete" onClick={() => deleletFile(i)}>
+                                                    <i className="bi bi-x fs-1"></i>
+                                                </span>
+                                            </div>
                                         </div>
-                                    ))}
+                                    </div>
+                                ))}
 
-                                </> : ''}
-                        </div>
+                            </> : ''}
+                    </div>
 
-                        <span className="form-text text-muted mt-2">Max file size is 1MB and max number of files is 5.</span>
-                    </>
-                )}
-            </Dropzone>
-        </>
+                    <span className="form-text text-muted mt-2">Max file size is 1MB and max number of files is 5.</span>
+                </>
+            )}
+        </Dropzone>
     )
 }
 
